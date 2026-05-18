fa66c03230f1dad1684297bda8ebd77d010c3da3
bekhruz-devv <alixonovbehruzbek.14@gmail.com>
2026-05-18 08:44:48 +0500

    update2

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import "../css/ProductDetail.css";
-import { products } from "../data/data.js";
+import { products } from "../data/products.js";
 
 const ProductDetail = () => {
   const { id } = useParams();
